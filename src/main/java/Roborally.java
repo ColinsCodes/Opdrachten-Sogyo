67363298a8a8b9c19ffbfcd65097d0461adb14d5
Colin Kasteleijn <ckasteleijn@sogyo.nl>
2026-07-08 16:34:10 +0200

    Roborally v2 incl execute-list

--- src/main/java/Roborally.java
+++ src/main/java/Roborally.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
-
+import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
 class Robot {
     int x;
     int y;
@@ -61,46 +63,80 @@ class Robot {
         }
     }
 }
+interface Command{
+    public void execute();          //dit deel snap ik niet zo goed. Volgens mij is het nodig zodat java IETS kan invullen in List<Command>
+}
+class executor {
+    private static final List<Command> commandList = new ArrayList<>();        //Creeert nieuwe lege arraylist genaamd commandlist
+
+    public static void commandAdd(Command command) {                    //maakt commando "commandAdd"
+        commandList.add(command);           //commandList is final en private en mag dus niet aangepast worden vanuit buiten deze module
+    }                                            //door deze line toe te voegen "outsource" je dat probleem naar dezelfde class
+    public static void executeALL() {           //weer binnen dezelfde class, een execute all commando
+        for (Command command : commandList) {   //voor alle units in de lijst
+            command.execute();                  //execute
+        }
+        commandList.clear();                    //gooi lijst leeg voor hergebruik
+    }
+}
 public class Roborally {
     public static void main(String[] args){
-        Robot robot1 = new Robot(0,1, Robot.Direction.West);
-        Robot robot2 = new Robot(1,0, Robot.Direction.Oost);
+        Robot robot1 = new Robot(0,1, Robot.Direction.West);        //definieert robot 1
+        Robot robot2 = new Robot(1,0, Robot.Direction.Oost);        //definieert robot 2
         while(true) {
-            System.out.println("U staat nu op (" + robot1.x + "," + robot1.y + ").");
-            System.out.println("U kijkt nu naar de richting \"" + robot1.direction + "\"");
-            System.out.println("Welke kant wilt U op? (links, rechts, rechtdoor, terug)");
-            Scanner action = new Scanner(System.in);
+            System.out.println("U staat nu op (" + robot1.x + "," + robot1.y + ").");   //geeft huidige positie weer
+            System.out.println("U kijkt nu naar de richting \"" + robot1.direction + "\""); //geeft huidige richting weer
+            System.out.println("Welke kant wilt U op? (links, rechts, rechtdoor, terug) Typ \"klaar\" om uw zetten te doen.");
+            Scanner action = new Scanner(System.in);        //setup user input
             Scanner speed = new Scanner(System.in);
-            String userInput = action.nextLine();
-            if (userInput.matches("links")) {
-                robot1.direction = robot1.direction.links();
-            } else if (userInput.matches("rechts")) {
-                robot1.direction = robot1.direction.rechts();
-            } else if (userInput.matches("rechtdoor")) {
-                System.out.println("Hoe snel? (1-3)");
-                String userType = speed.nextLine();
-                int userSpeed;
-                if(userType.isEmpty()){
-                    userSpeed = 1;
+            boolean loop = true;        //activeert loop
+            while (loop) {
+                String userInput = action.nextLine();       //input
+                if (userInput.matches("links")) {       //check input
+                    executor.commandAdd(() -> robot1.direction = robot1.direction.links());
+                } else if (userInput.matches("rechts")) {
+                    executor.commandAdd(() -> robot1.direction = robot1.direction.rechts());
+                } else if (userInput.matches("rechtdoor")) {
+                    boolean speedSet = false;
+                    while(!speedSet) {
+                        System.out.println("Hoe snel? (1-3)");      //check snelheid
+                        String userType = speed.nextLine();
+                        int userSpeed = 0;
+                        if (userType.isEmpty()) {
+                            userSpeed = 1;                          //zet speed 1 bij geen selectie
+                            speedSet = true;
+                        }else {
+                            try {
+                                userSpeed = Integer.parseInt(userType);
+                                if (userSpeed > 3) {                          //zet maximum snelheid 3
+                                    userSpeed = 3;
+                                }
+                                if (userSpeed < 1) {                          //zet minimum snelheid 1
+                                    userSpeed = 1;
+                                }
+                                while (userSpeed > 0) {                       //voert commando vooruit meerdere keren uit (lui)
+                                    executor.commandAdd(() -> robot1.forward()); //gooit het commando forward de lijst in
+                                    userSpeed = userSpeed - 1;
+                                }
+                                speedSet = true;
+                            } catch (NumberFormatException e) {
+                                System.out.println("Dat is geen nummer, probeer opnieuw.");
+                            }
+
+                        }
+                    }
+                } else if (userInput.matches("terug")) {
+                    executor.commandAdd(() -> robot1.backward());
+                } else if (userInput.matches("klaar")) {
+                    loop = false;
+                } else {
+                    System.out.println("Ongeldige zet, probeer opnieuw.");
                 }
-                else {
-                    userSpeed = Integer.parseInt(userType);
+                if(loop) {
+                    System.out.println("Volgende zet? Of typ \"klaar\"");
                 }
-                if(userSpeed > 3){
-                    userSpeed = 3;
-                }
-                if(userSpeed < 1){
-                    userSpeed = 1;
-                }
-                while(userSpeed > 0){
-                    robot1.forward();
-                    userSpeed = userSpeed - 1;
-                }
-            } else if (userInput.matches("terug")) {
-                robot1.backward();
-            } else{
-                System.out.println("Ongeldige zet, probeer opnieuw.");
             }
+        executor.executeALL(); //als de gebruiker "klaar" is, verstuurt alles tegelijk
         }
     }
 
